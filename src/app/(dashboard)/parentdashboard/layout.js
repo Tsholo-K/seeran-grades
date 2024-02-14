38f4afd6babaa082b9874ab2d8b578d4bc2f6813
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-15 00:04:57 +0200

    seeran limited

--- src/app/(dashboard)/parentdashboard/layout.js
+++ src/app/(dashboard)/parentdashboard/layout.js
@@ -10,18 +10,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`font-semibold antialiased bg-gray-100`}>
         {/* navigation bar */}  
-        <div className="fixed top-0 flex justify-center w-full pt-3 pb-1 bg-gray-100">
+        <div className="fixed top-0 flex justify-center w-full pt-3 pb-1 bg-gray-100 z-10">
             <div className="flex w-full px-4 sm:px-7 mx-auto">
                 <div className="w-full">
                     <p className=""><Image priority src={'/favicon.svg'} alt="hero image" width={0} height={0} className="h-[35px] w-[35px]"/></p>
                 </div>
             </div>
         </div>
-        {/* body */}
-        <div className="text-black pt-16 w-full bg-gray-100 px-3 lg:px-4 min-h-screen">
-            {children}
+        <div className="min-h-screen relative max-h-screen bg-gray-100 z-0">
+          {/* body */}
+          <div className="text-black pt-16 w-full px-3 lg:px-4">
+              {children}
+          </div>
+          <p className=" absolute bottom-0 w-full text-center text-gray-400 text-sm bg-gray-100 pb-3">seeran limited</p>
         </div>
-        <p className="w-full text-center text-gray-400 text-sm pb-4 pt-8 bg-gray-100 ">seeran limited</p>
+        
       </body>
     </html>
   );
