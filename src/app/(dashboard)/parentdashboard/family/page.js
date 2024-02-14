a9eade843e1905da562a463a11c9e20ca98bacde
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-15 00:33:43 +0200

    rework

--- src/app/(dashboard)/parentdashboard/family/page.js
+++ src/app/(dashboard)/parentdashboard/family/page.js
@@ -29,40 +29,43 @@ const MenuLink = ({Menu}) => {
 
 const page = () => {
   return (
-    <div className="w-full grid grid-cols-1 place-content-center ">
-        <div>
-            <h1 className="w-full text-center pb-16 text-4xl">Family</h1>
-        </div>
-        {/* accounts section */}
-        <div className="border rounded-xl bg-white">
-            <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
-                <Image priority src={'/profile-icon-2.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
-                <div className="pt-3 w-full">
-                    <p> Surname Name</p>
-                    <p className=" text-sm text-gray-500">grade: 5</p>
-                </div>
-                <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
-            </Link>
-            <hr className="mx-5"></hr>
-            <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
-                <Image priority src={'/profile-icon-3.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
-                <div className="pt-3 w-full">
-                    <p> Surname Name</p>
-                    <p className=" text-sm text-gray-500">grade: 7</p>
-                </div>
-                <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
-            </Link>
-            <hr className="mx-5"></hr>
-            <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
-                <Image priority src={'/profile-icon-4.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
-                <div className="pt-3 w-full">
-                    <p> Surname Name</p>
-                    <p className=" text-sm text-gray-500">grade: 2</p>
-                </div>
-                <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
-            </Link>
-        </div>
-        <p className=" text-sm w-[80%] text-gray-500 text-center mx-auto pt-2">these are all the children linked to your account</p>
+    <div className="w-full relative grid grid-cols-1 place-content-center">
+      <div className="absolute top-0 bg-gray-100 w-full">
+        <Link href={'/parentdashboard'} className="flex w-full"><Image src={'/chevron-left.svg'} alt='back to main menu' height={20} width={20}/>dashboard</Link>
+      </div>
+      <div >
+          <h1 className="w-full text-center pb-16 pt-10 text-4xl">Family</h1>
+      </div>
+      {/* accounts section */}
+      <div className="border rounded-xl bg-white pt">
+          <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
+              <Image priority src={'/profile-icon-2.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
+              <div className="pt-3 w-full">
+                  <p> Surname Name</p>
+                  <p className=" text-sm text-gray-500">grade: 5</p>
+              </div>
+              <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
+          </Link>
+          <hr className="mx-5"></hr>
+          <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
+              <Image priority src={'/profile-icon-3.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
+              <div className="pt-3 w-full">
+                  <p> Surname Name</p>
+                  <p className=" text-sm text-gray-500">grade: 7</p>
+              </div>
+              <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
+          </Link>
+          <hr className="mx-5"></hr>
+          <Link href={'/parentdashboard/profile'} className="flex py-2 gap-5 px-3 justify-start w-full cursor-pointer md:hover:text-blue-700">
+              <Image priority src={'/profile-icon-4.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-16 max-w-16" />
+              <div className="pt-3 w-full">
+                  <p> Surname Name</p>
+                  <p className=" text-sm text-gray-500">grade: 2</p>
+              </div>
+              <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit pt-5"/>
+          </Link>
+      </div>
+      <p className=" text-sm w-[80%] text-gray-500 text-center mx-auto pt-2">these are all the children linked to your account</p>
     </div>
   )
 }
