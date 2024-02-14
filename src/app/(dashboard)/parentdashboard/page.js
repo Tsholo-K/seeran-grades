55c8889c59a98226aa673d3076e644dc2f2a475b
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-14 20:08:07 +0200

    dashboard resize

--- src/app/(dashboard)/parentdashboard/page.js
+++ src/app/(dashboard)/parentdashboard/page.js
@@ -7,7 +7,7 @@ import ParentDashboardSidemenu from "@/components/parentdashboardsidemenu";
 
 const NavigationBar = () => {
   return (
-    <div className="fixed top-0 flex justify-center w-full py-3 bg-gray-100">
+    <div className="fixed top-0 flex justify-center w-full pt-3 pb-2 bg-gray-100">
       <div className="flex w-full px-4 sm:px-7 mx-auto">
         <div className="w-full">
           <Link href={'/parentdashboard'} className=""><Image priority src={'/favicon.svg'} alt="hero image" width={0} height={0} className="h-[35px] w-[35px]"/></Link>
@@ -19,7 +19,7 @@ const NavigationBar = () => {
 
 export default function ParentDashboard() {
     return (
-      <div className="text-black bg-gray-100 pt-16 md:pt-8 min-h-screen w-full">
+      <div className="text-black bg-gray-100 pt-10 min-h-screen w-full">
         {/* navigation bar */}
         <NavigationBar/>
         {/* dashboard */}
