aa186465fff1067894bce63787e8e5affdbe36bd
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-14 23:11:01 +0200

    profile settings

--- src/app/(dashboard)/parentdashboard/layout.js
+++ src/app/(dashboard)/parentdashboard/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         <div className="fixed top-0 flex justify-center w-full pt-3 pb-1 bg-gray-100">
             <div className="flex w-full px-4 sm:px-7 mx-auto">
                 <div className="w-full">
-                    <Link href={'/parentdashboard'} className=""><Image priority src={'/favicon.svg'} alt="hero image" width={0} height={0} className="h-[35px] w-[35px]"/></Link>
+                    <p className=""><Image priority src={'/favicon.svg'} alt="hero image" width={0} height={0} className="h-[35px] w-[35px]"/></p>
                 </div>
             </div>
         </div>
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <div className="text-black pt-16 min-h-screen w-full bg-gray-100 px-3 lg:px-4">
             {children}
         </div>
-        <p className="w-full text-center text-gray-400 text-sm mb-4 ">seeran limited</p>
+        <p className="w-full text-center text-gray-400 text-sm pb-4 bg-gray-100 ">seeran limited</p>
       </body>
     </html>
   );
--- src/app/(dashboard)/parentdashboard/profile/page.js
+++ src/app/(dashboard)/parentdashboard/profile/page.js
@@ -2,19 +2,52 @@ import Image from "next/image"
 import Link from "next/link"
 
 
+const Menu = {
+  personalinformation : {
+    title: 'Personal Information',
+    icon: 'book-lock',
+  },
+  signinandsecurity : {
+    title: 'Sign-in And Security',
+    icon: 'fingerprint',
+  },
+  signout : {
+    title: 'Sign Out',
+    icon: 'log-out',
+  }
+}
+
+const MenuLink = ({Menu}) => {
+  return (
+    <div className="flex gap-6 py-3 justify-start w-full px-3 cursor-pointer md:hover:text-blue-700">
+      <Image src={`/${Menu.icon}.svg`} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-10" />
+      <p className={`w-full`}>{Menu.title}</p>
+      <Image src={'/chevron-right-black.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit"/>
+    </div>
+  )
+}
+
 const page = () => {
   return (
     <div className="w-full">
-      <Link href={'/'} className="flex text-blue-700 sticky"><Image src={'/chevron-left.svg'} alt="back" width={30} height={30} className="w-fit h-fit max-h-6 max-w-6"/>Dashboard</Link>
+      <Link href={'/parentdashboard'} className="flex text-blue-700"><Image src={'/chevron-left.svg'} alt="back" width={30} height={30} className="w-fit h-fit max-h-6 max-w-6"/>Dashboard</Link>
       <div>
-        <h1 className="w-full text-center text-4xl">Profile</h1>
+        <h1 className="w-full text-center my-10 text-4xl">Profile Settings</h1>
       </div>
-      <div className="w-fit mx-auto">
+      <div className="w-fit my-10 mx-auto">
           <div className="mx-auto w-fit mb-4">
               <Image priority src={'/profile-icon-1.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-40 max-w-40" />
           </div>
           <h2 className="w-full text-center">Surname Name</h2>
           <p className="w-full text-center">email</p>
+      </div>
+      {/* children section */}
+      <div className="border rounded-xl my-7 bg-white">
+        <MenuLink Menu={Menu.personalinformation}/>
+        <hr className="mx-5"></hr>
+        <MenuLink Menu={Menu.signinandsecurity}/>
+        <hr className="mx-5"></hr>
+        <MenuLink Menu={Menu.signout}/>
       </div>
     </div>
   )
