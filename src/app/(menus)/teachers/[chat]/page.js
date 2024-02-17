6924e9ab085609e2bee96eaa011ff898178d5e1c
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-17 18:32:36 +0200

    chat rework

--- src/app/(menus)/teachers/[chat]/page.js
+++ src/app/(menus)/teachers/[chat]/page.js
@@ -30,22 +30,21 @@ const MenuLink = ({Menu}) => {
 const Teachers = () => {
   return (
     <div className="w-full min-h-full relative">
-        <div className="fixed flex top-0 pt-16 w-full pb-1 bg-gray-100">
-            <Link href={'/teachers'} className="flex text-blue-700">
-            <Image src={'/chevron-left.svg'} alt='back to main menu' height={20} width={20}/>17
+        <div className="fixed flex text-blue-700 top-0 pt-16 pb-1 bg-gray-100">
+            <Link href={'/teachers'}  className="flex">
+                <Image src={'/chevron-left.svg'} alt='back to main menu' height={20} width={20}/>
+                <p className="pt-3">17</p>
             </Link>
-        </div>
-        <div className="flex fixed top-0 pt-14 w-full justify-center">
-            <div className="flex w-fit gap-3 pt-1">
-                <Image priority src={'/profile-icon-1.svg'} alt="profile icon" width={30} height={30} className="w-fit h-fit max-h-8 max-w-8" />
+            <div className="flex pl-10 gap-3">
+                <Image priority src={'/profile-icon-1.svg'} alt="profile icon" width={35} height={35} className="w-fit h-fit max-h-12 max-w-12" />
                 <div className=" w-full">
-                    <p className="font-bold pt-1">Surname Name</p>
+                    <p className="font-bold pt-3 text-black">Surname Name</p>
                 </div>
             </div>
         </div>
-        <div className="fixed w-full bottom-0 left-0 mb-3 bg-gray-100 pt-1 pb-3 border-t-[1px] border-gray-200">
-            <div className="w-fit flex mx-auto gap-3">
-                <input type="text" className="w-fit  border-[1px] border-gray-300 rounded-full bg-white" />
+        <div className="fixed w-full bottom-0 left-0">
+            <div className="w-full flex justify-center gap-3 px-3 py-2">
+                <input type="text" className="w-full  border-[1px] p-1 border-gray-300 rounded-full bg-white" />
                 <Image src={'/send-horizontal.svg'} alt='back to main menu' height={26} width={26}/>
             </div>
         </div>
