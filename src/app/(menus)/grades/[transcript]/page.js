444519caf740f66add71c4d0ef1fa7612895e245
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-20 01:01:00 +0200

    grades

--- src/app/(menus)/grades/[transcript]/page.js
+++ src/app/(menus)/grades/[transcript]/page.js
@@ -1,13 +1,33 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
+
+// dummy data
+import data from '@/app/(menus)/dummydata.json'
 
 
-const Score = ({ transcript }) => {
+const Score = ({ score, total }) => {
+
+  let result = score / total * 100
+
+  let colour = null
+  if ( result > 79 ) {
+    colour = 'bg-green-600'
+  } else if ( 39 < result && result < 80 ) {
+    colour = 'bg-orange-500'
+  } else {
+    colour = 'bg-red-600'
+  }
+  
+  result = result.toString().slice(0, 4)
+  
   return (
     <>
       <div className="w-full pb-10">
         <div className="mx-auto w-fit">
-          <p className="bg-orange-500 text-center w-[150px] h-[150px] text-white rounded-full pt-[53px] text-5xl">52%</p>
+          <p className={`${colour} text-center w-[180px] h-[180px] text-white rounded-full pt-[67px] text-5xl`}>{result}%</p>
         </div>
         <p className="w-full text-center pt-3 text-gray-400 text-sm">score</p>
       </div>
@@ -16,7 +36,20 @@ const Score = ({ transcript }) => {
 };
 
 
-const Subject = ({ subject }) => {
+const SubmittedBy = ({ student_id }) => {
+
+  let submitter = ''
+  data.students.forEach( (student) => {
+    if ( student.student_number === student_id ) {
+      submitter = student
+    }
+  });
+  return (
+    <p className="w-full text-center pt-1 text-sm text-gray-400">submitted by : {submitter.name.charAt(0).toUpperCase() + submitter.name.slice(1)} {submitter.surname.charAt(0).toUpperCase() + submitter.surname.slice(1)}</p>
+  )
+}
+
+const Subject = ({ subject, icon, submitter }) => {
   return (
     <>
       <div className="py-4 mb-8">
@@ -25,48 +58,40 @@ const Subject = ({ subject }) => {
         </div>
         <div className="relative rounded-xl bg-white py-1">
           <div className="flex justify-center py-2 gap-1 px-3 w-full cursor-pointer md:hover:text-blue-700">
-              <Image priority src={'/life-science.svg'} alt="profile icon" width={30} height={30} className="h-8 w-8" />
+              <Image priority src={`/${icon}.svg`} alt="profile icon" width={30} height={30} className="h-8 w-8" />
               <div className="pt-1 pl-2">
-                  <p>Life Science</p>
+                  <p>{subject}</p>
               </div>
           </div>
         </div>
-        <p className="w-full text-center text-sm text-gray-400">submitted by ( Name )</p>
+        <SubmittedBy student_id={submitter}/>
       </div>
     </>
   )
 }
  
 
-const Menu = {
-  assessment : {
-    title: 'Assessment',
-    info: 'test',
-  },
-  grade : {
-    title: 'Mark',
-    info: '26/50',
-  },
-  date : {
-    title: 'Date Submitted',
-    info: '7 febuary 2024',
-  },
-  teacher : {
-    title: 'Teacher',
-    info: 'Mrs ..',
-  },
-};
-
-const MenuLink = ({Menu}) => {
+const MenuLink = ({ menu, info }) => {
   return (
     <div className="relative flex py-3 gap-1 justify-start w-full px-3 cursor-pointer md:hover:text-blue-700">
-      <p>{Menu.title}</p>
-      <p className="text-gray-400 absolute right-4">{Menu.info}</p>
+      <p>{menu}</p>
+      <p className="text-gray-400 absolute right-4">{info}</p>
     </div>
   )
 };
 
 const Transcript = () => {
+
+  const params = useParams();
+  const transcript_id = params.transcript
+
+  let transcript = null
+  data.grades.forEach(( grade ) => {
+    if ( grade.id === transcript_id ) {
+      transcript = grade
+    }
+  })
+
   return (
     <div className="w-full relative grid grid-cols-1 place-content-center ">
       {/* back link */}
@@ -80,23 +105,23 @@ const Transcript = () => {
         <h1 className="w-full text-center pb-16 text-4xl">Transcript</h1>
       </div>
       {/* score */}
-      <Score/>
+      <Score score={transcript.score} total={transcript.total}/>
       {/* subject */}
-      <Subject/>
+      <Subject subject={transcript.subject} icon={transcript.icon} submitter={transcript.submitted_by}/>
       {/* info */}
       <div className="rounded-xl bg-white mb-8">
-        <MenuLink Menu={Menu.assessment}/>
+        <MenuLink menu={'Assessment'} info={transcript.assessment}/>
         <hr className="mx-5"></hr>
-        <MenuLink Menu={Menu.grade}/>
+        <MenuLink menu={'Grade'} info={`${transcript.score} / ${transcript.total }`}/>
         <hr className="mx-5"></hr>
-        <MenuLink Menu={Menu.date}/>
+        <MenuLink menu={'Date'} info={transcript.submitted_by}/>
       </div>
       {/* teacher */}
       <div className="flex">
         <Link href={'/teachers/teacher'} className="pb-1 w-full text-end pr-3 text-blue-700">message</Link>
       </div>
       <div className="rounded-xl bg-white">
-        <MenuLink Menu={Menu.teacher}/>
+        <MenuLink menu={'Teacher ID'} info={transcript.teacher}/>
       </div>
     </div>
   )
--- src/app/(menus)/grades/page.js
+++ src/app/(menus)/grades/page.js
@@ -1,45 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 
+// dummy data
+import data from '@/app/(menus)/dummydata.json'
 
-const Transcripts= {
-  mark1: {
-    id: '1234',
-    name: 'Tsholo Koketso',
-    score: 'grades-ready-passed',
-    assessment: 'exam',
-    subject: 'Mathematics',
-    icon: 'maths',
-    date: 'tuesday, 14 january',
-  },
-  mark2: {
-    id: '12341',
-    name: 'Mochaki Lethabo',
-    score: 'grades-ready-okay',
-    assessment: 'class test',
-    subject: 'Life Science',
-    icon: 'life-science',
-    date: 'monday, 09 febuary',
-  },
-  mark3: {
-    id: '13413',
-    name: 'Mochaki Lethabo',
-    score: 'grades-ready-failed',
-    assessment: 'quiz',
-    subject: 'Physical Sciences',
-    icon: 'physics',
-    date: 'friday, 26 january',
-  },
-  mark4: {
-    id: '23412',
-    name: 'Munyoro Godwill',
-    score: 'grades-ready-okay',
-    assessment: 'practical',
-    subject: 'Mathematics',
-    icon: 'maths',
-    date: 'monday, 28 febuary',
-  },
-};
+
+const SubmittedBy = ({ student_id }) => {
+
+  let submitter = ''
+  data.students.forEach( (student) => {
+    if ( student.student_number === student_id ) {
+      submitter = student
+    }
+  });
+  return (
+    <p className=" text-sm pl-2 pb-2 text-gray-500">{submitter.name.charAt(0).toUpperCase() + submitter.name.slice(1)} {submitter.surname.charAt(0).toUpperCase() + submitter.surname.slice(1)}</p>
+  )
+}
+
+const TranscriptScore = ({ score, total }) => {
+
+  const result = score / total * 100
+  let mark
+  if( result > -1 ) {
+    mark = 'grades-ready-failed'
+  }
+  if( result > 39 ) {
+    mark = 'grades-ready-okay'
+  }
+  if( result > 79 ) {
+    mark = 'grades-ready-passed'
+  }
+
+  return (
+    <Image src={`/${mark}.svg`} alt='try it button' width={10} height={10} className="absolute w-fit h-fit top-2 right-3"/>
+  )
+}
 
 const Grade = ({ transcript }) => {
   return (
@@ -47,23 +43,23 @@ const Grade = ({ transcript }) => {
       <div className="py-4">
         <div className="flex pl-2">
           <Image src={'/pencil-line.svg'} alt='try it button' width={10} height={10} className="w-5 h-5"/>
-          <p className=" text-sm pl-2 pb-2 text-gray-500">{transcript.name}</p>
+          <SubmittedBy student_id={transcript.submitted_by}/>
         </div>
         <Link href={`/grades/${transcript.id}`}>
           <div className="relative rounded-xl bg-white py-1" >
-            <Image src={`/${transcript.score}.svg`} alt='try it button' width={10} height={10} className="absolute w-fit h-fit top-2 right-3"/>
+            <TranscriptScore score={transcript.score} total={transcript.total}/>
             <div>
               <p className="text-sm text-gray-500 pt-1 w-full text-center">{transcript.assessment}</p>
             </div>
             <div className="flex justify-center py-2 gap-1 px-3 w-full cursor-pointer md:hover:text-blue-700">
                 <Image priority src={`/${transcript.icon}.svg`} alt="profile icon" width={30} height={30} className="h-8 w-8" />
                 <div className="pt-1 pl-2">
-                    <p>{transcript.subject}</p>
+                    <p>{transcript.subject.charAt(0).toUpperCase() + transcript.subject.slice(1)}</p>
                 </div>
             </div>
             <div className="flex pl-3 pt-2">
               <Image src={'/date.svg'} alt='try it button' width={10} height={10} className="w-5 h-5"/>
-              <p className=" text-sm pl-2 pb-2 text-gray-500">released : {transcript.date}</p>
+              <p className=" text-sm pl-2 pb-2 text-gray-500">released : {transcript.date_released}</p>
               <p className=" text-sm pl-2 pb-2 text-gray-500"></p>
             </div>
           </div>
@@ -89,12 +85,10 @@ const Grades = () => {
       </div>
       {/* transcripts */}
       {
-        
+        data.grades.map( (transcript, index) => (
+          <Grade key={index} transcript={transcript} />
+        ))
       }
-      <Grade transcript={Transcripts.mark1}/>
-      <Grade transcript={Transcripts.mark2}/>
-      <Grade transcript={Transcripts.mark4}/>
-      <Grade transcript={Transcripts.mark3}/>
       <p className=" text-sm w-[80%] text-gray-500 text-center mx-auto pt-5">all caught up..</p>
     </div>
   )
