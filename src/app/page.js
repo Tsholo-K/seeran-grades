a9dda72ad1329f39086dbf3a599ef5af2e0d742f
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-22 23:36:38 +0200

    okay

--- src/app/page.js
+++ src/app/page.js
@@ -61,116 +61,25 @@ export default function Home() {
           paragraph={'Effective communication between schools, students, parents and teachers is vital for academic success. Seeran Grades bridges this gap, offering a robust platform for seamless interaction and academic monitoring.'}
           image={'second-hero'}
           side={'left'} 
-
         />
         <Section 
           heading={"key features"}
-          paragraph={'Effective communication between schools, students, parents and teachers is vital for academic success. Seeran Grades bridges this gap, offering a robust platform for seamless interaction and academic monitoring.'}
+          paragraph={`Student and Parent Portal: Access to grades, assignments, announcements, and school events. Teacher Dashboard: Grade submissions, communication with students and parents, and assignment tracking. Mobile Accessibility: Accessible anytime, anywhere`}
           image={'key-features'}
           side={'right'} 
-
+        />
+         <Section 
+          heading={"But... why seeran grades"}
+          paragraph={`
+            seeran grades offers a transformative solution to the age-old challenge of effective communication and academic management in 
+            educational institutions. By providing a centralized platform accessible to students, parents, teachers, and school administrators.
+            seeran grades revolutionizes the way we interact, collaborate, and monitor academic progress. 
+          `}
+          image={'why-seeran'}
+          side={'left'} 
         />
       </div>
+      <Footer/>
     </main>
   );
-}
-
-
-const nothing = ( ) => {
-  return (
-    <>
-      <div className="w-full px-3 sm:px-10 md:px-14 mb-10 mx-auto">
-            <div className="pt-32">
-            </div>
-            {/* how it works section */}
-            <p className="w-full text-center text-base">How it works</p>
-            <div className="flex w-full justify-center">
-              <Image src={'/chevron-down.svg'} alt='try it button' width={10} height={10} className="w-fit h-fit"/>
-            </div>
-            <hr className="my-3"></hr>
-            {/* opening paragraph section */}
-            <div>
-              <div>
-                <h2 className="mb-10 text-[40px] sm:text-5xl text-center">seeran <span className="text-blue-700">grades</span></h2>
-              </div>
-              <div className="lg:flex w-full">
-                <Image src={'/second-hero.svg'} alt="seeran grades hero image" height={0} width={0} className="w-fit h-fit mx-auto max-h-[200px] lg:max-h-[450px]"/>
-                <div className="lg:w-full">
-                  <p className="w-full text-center">
-                    In today&apos;s digital age, effective communication between schools, students, parents and teachers is vital for academic success. 
-                    Seeran Grades bridges this gap, offering a robust platform for seamless interaction and academic monitoring.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <hr className="my-10"></hr>
-            {/* key features section */}
-            <div>
-              <div>
-                  <h2 className="mt-10 mb-11 text-3xl text-center sm:text-4xl"><span className="text-blue-700">Key </span>Features</h2>
-              </div>
-              <div className="lg:flex w-full">
-                <div className="lg:grid grid-cols-1 place-content-center">
-                  <p className="w-full text-center my-4">Student and Parent Portal: Access to grades, assignments, announcements, and school events.</p>
-                  <p className="w-full text-center my-4">Teacher Dashboard: Grade submissions, communication with students and parents, and assignment tracking.</p>
-                  <p className="w-full text-center my-4">Mobile Accessibility: Accessible anytime, anywhere</p>
-                </div>
-                <Image src={'/key-features.svg'} alt="seeran grades hero image" height={0} width={0} className="w-full h-full mb-4 max-h-[400px] lg:max-h-[600px]"/>
-              </div>
-            </div>
-            <hr className="my-10"></hr>
-            {/* Student and Parent Portal section */}
-            <div>
-              <div>
-                <h2 className="my-10 text-3xl text-center sm:text-4xl">Student and Parent <br className="md:hidden"></br><span className="text-blue-700">Portal</span></h2> 
-              </div>
-              <div className="lg:flex w-full">
-                <Image src={'/student-parent-portal.svg'} alt="seeran grades hero image" height={0} width={0} className="w-full h-full mb-4 max-h-[400px] lg:max-h-[600px]"/>
-                <div className="lg:grid grid-cols-1 place-content-center">
-                  <p className="w-full text-center my-4">Assignments: Details of upcoming assignments, deadlines, and submission status.</p>
-                  <p className="w-full text-center my-4">Announcements: Important updates and announcements from the school..</p>
-                  <p className="w-full text-center my-4">Messaging: Direct communication with teachers and school administrators.</p>
-                </div>
-              </div>
-            </div>
-            <hr className="my-10"></hr>
-            {/* teacher dashboard section */}
-            <div>
-              <div>
-                <h2 className="my-10 text-3xl text-center sm:text-4xl">Teacher <span className="text-blue-700">Dashboard</span></h2>
-              </div>
-              <div className="lg:flex w-full">
-                <div className="lg:grid grid-cols-1 place-content-center">
-                  <p className="w-full text-center my-4">Grade Submission: Effortless submission and tracking of grades for each student.</p>
-                  <p className="w-full text-center my-4">Communication: Direct messaging with parents for feedback and updates.</p>
-                  <p className="w-full text-center my-4">Assignment Tracking: Monitoring assignment progress and providing timely feedback.</p>
-                </div>
-                <Image src={'/teacher-dashboard.svg'} alt="seeran grades hero image" height={0} width={0} className="w-full h-full mb-4 max-h-[400px] lg:max-h-[600px]"/>
-              </div>
-            </div>
-            <hr className="my-10"></hr>
-            {/* but why seeran section */}
-            <div>
-              <h2 className="my-10 text-3xl text-center sm:text-4xl">
-                But... why <br className="md:hidden"></br>seeran <span className="text-blue-700">grades</span>
-              </h2>
-              <Image src={'/why-seeran.svg'} alt="seeran grades hero image" height={0} width={0} className="w-full h-full mb-4 max-h-[400px] lg:max-h-[600px]"/>
-              <p className="w-full text-center my-4">
-                seeran grades offers a transformative solution to the age-old challenge of effective communication and academic management in educational
-                institutions. 
-                By providing a centralized platform accessible to students, parents, teachers, and school administrators.
-                seeran grades revolutionizes the way we interact, collaborate, and monitor academic progress. Parents are empowered with real-time access
-                to their child&apos;s grades and assignments, fostering active involvement in their educational journey.
-                Teachers benefit from streamlined administrative tasks and enhanced 
-                communication channels, enabling them to provide timely feedback and support to students and parents.
-                With Seeran Grades, education becomes a collaborative effort where
-                communication meets academic achievement, laying the foundation for a brighter future for students and schools alike.
-              </p>
-            </div>
-      </div>
-      {/* footer */}
-      <Footer/>
-    </>
-      
-  )
 }
