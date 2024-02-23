5a04d794352a96890ffd36fb7a46a22b96cca18c
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-23 20:22:13 +0200

    text order

--- src/app/(authentication)/mfa/page.js
+++ src/app/(authentication)/mfa/page.js
@@ -22,7 +22,7 @@ const Signin = () => {
     <>
       <div className="h-screen relative">
         <div className="w-full h-[60%] max-w-screen-xl mx-auto grid grid-cols-1 place-content-center">
-          <p className="w-full text-center text-sm md:text-base">we sent a one time <span className="text-[#66b0f0]">passcode </span>to your corresponding number/email</p>
+          <p className="w-full text-center text-sm md:text-base">we sent a one time <span className="text-[#66b0f0]">passcode </span><br className="md:hidden"></br>to your corresponding number/email</p>
           <form className="mx-auto w-[80%] lg:w-[60%]" onSubmit={authenticate}>
             <div className="my-4">
               <Input type={'text'} placeholder={'OTP'} />
